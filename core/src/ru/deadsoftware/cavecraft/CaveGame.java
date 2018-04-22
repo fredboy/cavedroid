6d02313555a768027186198366abf213b0557fb5
fred-boy <fred-boy@protonmail.com>
2018-04-22 23:17:49 +0700

    Update version

--- core/src/ru/deadsoftware/cavecraft/CaveGame.java
+++ core/src/ru/deadsoftware/cavecraft/CaveGame.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Game;
 
 public class CaveGame extends Game {
 
-	public static final String VERSION = "alpha 0.2";
+	public static final String VERSION = "alpha 0.2.1";
 
 	public static GameState STATE;
 
