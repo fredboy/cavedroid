45774fe0e54d63043d505867137df6b7e60da690
fredboy <fredboy@protonmail.com>
2024-04-17 12:27:03 +0700

    Add attribution to so script

--- gen-changelog.sh
+++ gen-changelog.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Waku-2 CC BY-SA 3.0
+# https://stackoverflow.com/a/46033999
+# https://creativecommons.org/licenses/by-sa/3.0/
+
 previous_tag=0
 i=0
 for current_tag in $(git tag --sort=-creatordate)
