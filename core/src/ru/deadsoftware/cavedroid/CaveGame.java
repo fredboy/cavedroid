533a7e5290163854e2af1f6f54b835d6fe766108
fredboy <fredboy@protonmail.com>
2024-04-22 16:26:06 +0700

    Update version

--- core/src/ru/deadsoftware/cavedroid/CaveGame.java
+++ core/src/ru/deadsoftware/cavedroid/CaveGame.java
@@ -13,7 +13,7 @@ public class CaveGame extends Game {
 
     private static final String TAG = "CaveGame";
 
-    public static final String VERSION = "alpha 0.6.0";
+    public static final String VERSION = "alpha 0.6.1";
 
     private final MainConfig mMainConfig;
     private final MainComponent mMainComponent;
