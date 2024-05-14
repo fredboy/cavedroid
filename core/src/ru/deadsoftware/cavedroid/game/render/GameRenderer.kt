ca67da5ca90819bdbf2f0cda78d28456a2b58d76
fredboy <fredboy@protonmail.com>
2024-05-14 23:16:13 +0700

    Move IGameRenderer

--- core/src/ru/deadsoftware/cavedroid/game/render/GameRenderer.kt
+++ core/src/ru/deadsoftware/cavedroid/game/render/GameRenderer.kt
@@ -1,5 +1,8 @@
 package ru.deadsoftware.cavedroid.game.render
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.Rectangle
 import ru.fredboy.cavedroid.ksp.annotations.GenerateSetMultibindingsModule
 
 @GenerateSetMultibindingsModule(
@@ -8,3 +11,18 @@ import ru.fredboy.cavedroid.ksp.annotations.GenerateSetMultibindingsModule
     moduleName = "RenderModule"
 )
 annotation class GameRenderer
+
+interface IGameRenderer {
+
+    val renderLayer: Int
+
+    /**
+     * When called, [spriteBatch] is beginned!
+     */
+    fun draw(
+        spriteBatch: SpriteBatch,
+        shapeRenderer: ShapeRenderer,
+        viewport: Rectangle,
+        delta: Float
+    )
+}
