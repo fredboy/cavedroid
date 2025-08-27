b463c314df195686079072876ac60d0620e9b504
fredboy <fredboy@protonmail.com>
2025-08-28 05:18:11 +0700

    Update version

--- buildSrc/src/main/kotlin/ApplicationInfo.kt
+++ buildSrc/src/main/kotlin/ApplicationInfo.kt
@@ -2,8 +2,8 @@ import org.gradle.api.JavaVersion
 
 object ApplicationInfo {
     const val name = "CaveDroid"
-    const val versionName = "beta-0.10.0"
-    const val versionCode = 29
+    const val versionName = "beta-0.11.0"
+    const val versionCode = 30
 
     const val packageName = "ru.fredboy.cavedroid"
 
