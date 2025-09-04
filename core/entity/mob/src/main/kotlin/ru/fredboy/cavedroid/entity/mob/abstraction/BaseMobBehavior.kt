305f6a549b4afb88a11d1239fc1e8a9ab9dae99f
fredboy <fredboy@protonmail.com>
2025-09-04 22:16:47 +0700

    Fix auto jumping on walls

--- core/entity/mob/src/main/kotlin/ru/fredboy/cavedroid/entity/mob/abstraction/BaseMobBehavior.kt
+++ core/entity/mob/src/main/kotlin/ru/fredboy/cavedroid/entity/mob/abstraction/BaseMobBehavior.kt
@@ -1,5 +1,6 @@
 package ru.fredboy.cavedroid.entity.mob.abstraction
 
+import ru.fredboy.cavedroid.common.utils.forEachBlockInArea
 import ru.fredboy.cavedroid.entity.mob.model.Direction
 import ru.fredboy.cavedroid.entity.mob.model.Mob
 import kotlin.reflect.KClass
@@ -22,13 +23,30 @@ abstract class BaseMobBehavior<MOB : Mob>(
     }
 
     open fun MOB.updateMob(worldAdapter: MobWorldAdapter, playerAdapter: PlayerAdapter, delta: Float) {
-        if (checkForAutojump()) {
+        if (checkForAutojump(worldAdapter)) {
             jump()
         }
     }
 
-    open fun MOB.checkForAutojump(): Boolean {
-        return controlVector.x != 0f && autojumpCounters[Direction.fromVector(controlVector).index] > 0
+    open fun MOB.checkForAutojump(worldAdapter: MobWorldAdapter): Boolean {
+        return controlVector.x != 0f && autojumpCounters[Direction.fromVector(controlVector).index] > 0 &&
+            checkAutojumpObstacle(worldAdapter)
+    }
+
+    private fun MOB.checkAutojumpObstacle(worldAdapter: MobWorldAdapter): Boolean {
+        val targetHitbox = hitbox.apply {
+            x += direction.basis
+            y -= 1f
+        }
+
+        var hasObstacle = false
+        forEachBlockInArea(targetHitbox) { x, y ->
+            val block = worldAdapter.getForegroundBlock(x, y)
+            if (block.params.hasCollision && targetHitbox.overlaps(block.getRectangle(x, y))) {
+                hasObstacle = true
+            }
+        }
+        return !hasObstacle
     }
 
     companion object {
