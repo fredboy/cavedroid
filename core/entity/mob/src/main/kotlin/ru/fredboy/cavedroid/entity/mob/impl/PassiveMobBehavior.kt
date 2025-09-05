ea3c44ddfeb8a4b50183622861d896990c2d3089
fredboy <fredboy@protonmail.com>
2025-09-05 23:45:12 +0700

    "Change passive mobs behavior"

--- core/entity/mob/src/main/kotlin/ru/fredboy/cavedroid/entity/mob/impl/PassiveMobBehavior.kt
+++ core/entity/mob/src/main/kotlin/ru/fredboy/cavedroid/entity/mob/impl/PassiveMobBehavior.kt
@@ -4,25 +4,60 @@ import com.badlogic.gdx.math.MathUtils
 import ru.fredboy.cavedroid.entity.mob.abstraction.BaseMobBehavior
 import ru.fredboy.cavedroid.entity.mob.abstraction.MobWorldAdapter
 import ru.fredboy.cavedroid.entity.mob.abstraction.PlayerAdapter
+import ru.fredboy.cavedroid.entity.mob.model.Direction
 import ru.fredboy.cavedroid.entity.mob.model.SheepMob
 import ru.fredboy.cavedroid.entity.mob.model.WalkingMob
+import kotlin.math.abs
 
 class PassiveMobBehavior :
     BaseMobBehavior<WalkingMob>(
         mobType = WalkingMob::class,
     ) {
 
+    private var targetCoordinates: Pair<Int, Int>? = null
+
+    private fun WalkingMob.getTargetBlock(worldAdapter: MobWorldAdapter): Pair<Int, Int>? {
+        val x = ((position.x - WALK_RADIUS).toInt()..(position.x + WALK_RADIUS).toInt()).random()
+        var y = position.y.toInt() + 2
+        while (y > position.y - 2) {
+            if (worldAdapter.getForegroundBlock(x, y).params.hasCollision &&
+                !worldAdapter.getForegroundBlock(x, y - 1).params.hasCollision
+            ) {
+                break
+            }
+            y--
+        }
+
+        if (worldAdapter.getForegroundBlock(x, y - 1).params.hasCollision) {
+            return null
+        }
+
+        return x to y
+    }
+
     override fun WalkingMob.updateMob(worldAdapter: MobWorldAdapter, playerAdapter: PlayerAdapter, delta: Float) {
         if (checkForAutojump(worldAdapter)) {
             jump()
         }
 
-        if (MathUtils.randomBoolean(delta)) {
-            if (velocity.x != 0f) {
-                velocity.x = 0f
+        if (targetCoordinates == null && (takingDamage || MathUtils.randomBoolean(0.001f))) {
+            targetCoordinates = getTargetBlock(worldAdapter)
+        }
+
+        targetCoordinates?.let { (targetX, _) ->
+            if (abs(targetX - position.x) < 1) {
+                targetCoordinates = null
             } else {
-                changeDir()
+                direction = if (targetX > position.x) {
+                    Direction.RIGHT
+                } else {
+                    Direction.LEFT
+                }
+
+                controlVector.x = speed * direction.basis
             }
+        } ?: run {
+            controlVector.setZero()
         }
 
         climb = canSwim
@@ -30,5 +65,9 @@ class PassiveMobBehavior :
         if (this is SheepMob && MathUtils.randomBoolean(0.0001f)) {
             hasFur = true
         }
+    }
+
+    companion object {
+        private const val WALK_RADIUS = 8f
     }
 }
