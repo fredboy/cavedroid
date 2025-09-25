2d5f6d165b81291ba4465daa873931c440820ab1
fredboy <fredboy@protonmail.com>
2025-09-25 20:15:20 +0700

    Add arch to release archives

--- make-release.sh
+++ make-release.sh
@@ -18,8 +18,8 @@ mkdir "$release_dir"
 ./gradlew clean ktlintCheck android:assembleRelease desktop:packageLinuxX64 desktop:packageWinX64
 
 cp "android/build/outputs/apk/release/android-release.apk" "$release_dir/android-$1.apk"
-cp "desktop/build/dist/cavedroid-linuxX64.zip" "$release_dir/linux-$1.zip"
-cp "desktop/build/dist/cavedroid-winX64.zip" "$release_dir/win-$1.zip"
+cp "desktop/build/dist/cavedroid-linuxX64.zip" "$release_dir/linux-x86_64-$1.zip"
+cp "desktop/build/dist/cavedroid-winX64.zip" "$release_dir/win-x86_64-$1.zip"
 
 git tag "$1"
 
