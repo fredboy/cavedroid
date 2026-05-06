a3662d460138b037114e7ff7ea11b4ec6d7f0bbd
fredboy <fredboy@protonmail.com>
2026-05-07 01:40:57 +0700

    Add debounce on resize

--- desktop/src/main/kotlin/ru/fredboy/cavedroid/desktop/utils/SaveSizePrefsGameDecorator.kt
+++ desktop/src/main/kotlin/ru/fredboy/cavedroid/desktop/utils/SaveSizePrefsGameDecorator.kt
@@ -3,28 +3,35 @@ package ru.fredboy.cavedroid.desktop.utils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import ru.fredboy.cavedroid.common.CaveDroidConstants
+import ru.fredboy.cavedroid.common.coroutines.GdxMainDispatcher
 import ru.fredboy.cavedroid.gdx.CaveDroidApplication
 import ru.fredboy.cavedroid.gdx.CaveDroidApplicationDecorator
+import kotlin.time.Duration.Companion.seconds
 
 class SaveSizePrefsGameDecorator(
     private val delegate: CaveDroidApplication,
 ) : CaveDroidApplicationDecorator by delegate {
 
     private val job = Job()
-    private val scope = CoroutineScope(job + Dispatchers.Default)
+    private val scope = CoroutineScope(job + GdxMainDispatcher)
 
     private val preferencesStore get() = delegate.getPreferencesStore()
 
+    private val resizeEventFlow = MutableSharedFlow<Pair<Int, Int>>()
+
+    override fun create() {
+        observeResize()
+        delegate.create()
+    }
+
     override fun resize(width: Int, height: Int) {
         scope.launch {
-            logger.d { "Saving size preference: $width x $height" }
-            withContext(Dispatchers.IO) {
-                preferencesStore.setPreference(CaveDroidConstants.PreferenceKeys.WINDOW_WIDTH_KEY, width.toString())
-                preferencesStore.setPreference(CaveDroidConstants.PreferenceKeys.WINDOW_HEIGHT_KEY, height.toString())
-            }
+            resizeEventFlow.emit(Pair(width, height))
         }
 
         delegate.resize(width, height)
@@ -33,6 +40,26 @@ class SaveSizePrefsGameDecorator(
     override fun dispose() {
         job.cancel()
         delegate.dispose()
+    }
+
+    private fun observeResize() {
+        scope.launch {
+            resizeEventFlow
+                .debounce(1.seconds)
+                .collect { (width, height) ->
+                    logger.d { "Saving size preference: $width x $height" }
+                    withContext(Dispatchers.IO) {
+                        preferencesStore.setPreference(
+                            key = CaveDroidConstants.PreferenceKeys.WINDOW_WIDTH_KEY,
+                            value = width.toString()
+                        )
+                        preferencesStore.setPreference(
+                            key = CaveDroidConstants.PreferenceKeys.WINDOW_HEIGHT_KEY,
+                            value = height.toString()
+                        )
+                    }
+                }
+        }
     }
 
     companion object {
