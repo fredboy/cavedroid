162d1ecfaeb02cb25874f94038a63ce3baa5c075
fredboy <fredboy@protonmail.com>
2026-05-15 00:23:59 +0700

    #44: Scrollable language view

--- core/gdx/src/main/kotlin/ru/fredboy/cavedroid/gdx/menu/v2/view/language/LanguageMenuView.kt
+++ core/gdx/src/main/kotlin/ru/fredboy/cavedroid/gdx/menu/v2/view/language/LanguageMenuView.kt
@@ -1,33 +1,76 @@
 package ru.fredboy.cavedroid.gdx.menu.v2.view.language
 
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
+import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable
+import com.badlogic.gdx.utils.Align
 import ktx.scene2d.Scene2dDsl
 import ktx.scene2d.actors
+import ktx.scene2d.label
+import ktx.scene2d.scrollPane
+import ktx.scene2d.table
 import ktx.scene2d.textButton
 import ru.fredboy.cavedroid.common.utils.startWithCapital
-import ru.fredboy.cavedroid.gdx.menu.v2.view.common.menuButtonsTable
 import ru.fredboy.cavedroid.gdx.menu.v2.view.common.onClickWithSound
 
 @Scene2dDsl
 fun Stage.languageMenuView(viewModel: LanguageMenuViewModel) = viewModel.also {
     actors {
-        menuButtonsTable {
-            viewModel.locales.map { locale ->
-                textButton(locale.getDisplayLanguage(locale).startWithCapital(locale)) {
-                    onClickWithSound(viewModel) {
-                        viewModel.onLanguageSelect(locale)
+        table {
+            setFillParent(true)
+            background(
+                TiledDrawable(
+                    TextureRegionDrawable(
+                        skin.getRegion("background"),
+                    ),
+                ),
+            )
+            pad(8f)
+
+            label(viewModel.getLocalizedString("settings")) {
+                setAlignment(Align.center)
+            }.cell(
+                expandX = true,
+                fillX = true,
+                padBottom = 8f,
+            )
+
+            row()
+
+            scrollPane {
+                table {
+                    defaults()
+                        .width(600f)
+                        .height(60f)
+                        .pad(10f)
+
+                    viewModel.locales.map { locale ->
+                        textButton(locale.getDisplayLanguage(locale).startWithCapital(locale)) {
+                            onClickWithSound(viewModel) {
+                                viewModel.onLanguageSelect(locale)
+                            }
+                        }
+
+                        row()
                     }
                 }
-
-                row()
-            }
-
+            }.also { pane ->
+                setScrollFocus(pane)
+            }.cell(
+                expand = true,
+                fill = true,
+                align = Align.center,
+            )
             row()
                 .bottom()
 
             textButton(viewModel.getLocalizedString("back")) {
                 onClickWithSound(viewModel) { viewModel.onBackClick() }
-            }
+            }.cell(
+                width = 600f,
+                height = 60f,
+                padTop = 16f,
+            )
         }
     }
 }
