fec0c2d250e6880f1623506c89bcd0f6272a6fe2
fredboy <fredboy@protonmail.com>
2026-05-20 14:25:43 +0700

    Cake eating stages

--- core/gameplay/controls/src/main/kotlin/ru/fredboy/cavedroid/gameplay/controls/action/useblock/UseCakeAction.kt
+++ core/gameplay/controls/src/main/kotlin/ru/fredboy/cavedroid/gameplay/controls/action/useblock/UseCakeAction.kt
@@ -2,17 +2,17 @@ package ru.fredboy.cavedroid.gameplay.controls.action.useblock
 
 import ru.fredboy.cavedroid.common.di.GameScope
 import ru.fredboy.cavedroid.domain.items.model.block.Block
+import ru.fredboy.cavedroid.domain.items.usecase.GetBlockByKeyUseCase
 import ru.fredboy.cavedroid.entity.mob.model.Player
 import ru.fredboy.cavedroid.game.controller.mob.MobController
 import ru.fredboy.cavedroid.game.world.GameWorld
 import ru.fredboy.cavedroid.gameplay.controls.action.annotation.BindUseBlockAction
 import javax.inject.Inject
 
-@GameScope
-@BindUseBlockAction(stringKey = UseCakeAction.KEY)
-class UseCakeAction @Inject constructor(
+abstract class UseCakeAction(
     private val gameWorld: GameWorld,
     private val mobController: MobController,
+    private val getBlockByKeyUseCase: GetBlockByKeyUseCase,
 ) : IUseBlockAction {
 
     override fun perform(block: Block, x: Int, y: Int): Boolean {
@@ -20,13 +20,115 @@ class UseCakeAction @Inject constructor(
             return false
         }
         mobController.player.eat(HEAL, SATURATION)
-        gameWorld.resetForeMap(x, y)
+
+        val nextCake = CAKE_NEXT[block.params.key]?.let { key ->
+            getBlockByKeyUseCase[key]
+        }
+
+        if (nextCake != null) {
+            gameWorld.setForeMap(x, y, nextCake)
+        } else {
+            gameWorld.resetForeMap(x, y)
+        }
+
         return true
     }
 
     companion object {
-        const val KEY = "cake"
-        private const val HEAL = 14
-        private const val SATURATION = 2.8f
+        private const val HEAL = 2
+        private const val SATURATION = 0.4f
+
+        private val CAKE_NEXT = mapOf(
+            "cake_0" to "cake_1",
+            "cake_1" to "cake_2",
+            "cake_2" to "cake_3",
+            "cake_3" to "cake_4",
+            "cake_4" to "cake_5",
+            "cake_5" to "cake_6",
+        )
     }
 }
+
+@GameScope
+@BindUseBlockAction(stringKey = "cake_0")
+class UseCake0Action @Inject constructor(
+    gameWorld: GameWorld,
+    mobController: MobController,
+    getBlockByKeyUseCase: GetBlockByKeyUseCase,
+) : UseCakeAction(
+    gameWorld = gameWorld,
+    mobController = mobController,
+    getBlockByKeyUseCase = getBlockByKeyUseCase,
+)
+
+@GameScope
+@BindUseBlockAction(stringKey = "cake_1")
+class UseCake1Action @Inject constructor(
+    gameWorld: GameWorld,
+    mobController: MobController,
+    getBlockByKeyUseCase: GetBlockByKeyUseCase,
+) : UseCakeAction(
+    gameWorld = gameWorld,
+    mobController = mobController,
+    getBlockByKeyUseCase = getBlockByKeyUseCase,
+)
+
+@GameScope
+@BindUseBlockAction(stringKey = "cake_2")
+class UseCake2Action @Inject constructor(
+    gameWorld: GameWorld,
+    mobController: MobController,
+    getBlockByKeyUseCase: GetBlockByKeyUseCase,
+) : UseCakeAction(
+    gameWorld = gameWorld,
+    mobController = mobController,
+    getBlockByKeyUseCase = getBlockByKeyUseCase,
+)
+
+@GameScope
+@BindUseBlockAction(stringKey = "cake_3")
+class UseCake3Action @Inject constructor(
+    gameWorld: GameWorld,
+    mobController: MobController,
+    getBlockByKeyUseCase: GetBlockByKeyUseCase,
+) : UseCakeAction(
+    gameWorld = gameWorld,
+    mobController = mobController,
+    getBlockByKeyUseCase = getBlockByKeyUseCase,
+)
+
+@GameScope
+@BindUseBlockAction(stringKey = "cake_4")
+class UseCake4Action @Inject constructor(
+    gameWorld: GameWorld,
+    mobController: MobController,
+    getBlockByKeyUseCase: GetBlockByKeyUseCase,
+) : UseCakeAction(
+    gameWorld = gameWorld,
+    mobController = mobController,
+    getBlockByKeyUseCase = getBlockByKeyUseCase,
+)
+
+@GameScope
+@BindUseBlockAction(stringKey = "cake_5")
+class UseCake5Action @Inject constructor(
+    gameWorld: GameWorld,
+    mobController: MobController,
+    getBlockByKeyUseCase: GetBlockByKeyUseCase,
+) : UseCakeAction(
+    gameWorld = gameWorld,
+    mobController = mobController,
+    getBlockByKeyUseCase = getBlockByKeyUseCase,
+)
+
+@GameScope
+@BindUseBlockAction(stringKey = "cake_6")
+class UseCake6Action @Inject constructor(
+    gameWorld: GameWorld,
+    mobController: MobController,
+    getBlockByKeyUseCase: GetBlockByKeyUseCase,
+) : UseCakeAction(
+    gameWorld = gameWorld,
+    mobController = mobController,
+    getBlockByKeyUseCase = getBlockByKeyUseCase,
+)
---
+++ core/gameplay/controls/src/main/kotlin/ru/fredboy/cavedroid/gameplay/controls/action/useitem/UseCakeAction.kt
@@ -0,0 +1,34 @@
+package ru.fredboy.cavedroid.gameplay.controls.action.useitem
+
+import ru.fredboy.cavedroid.common.di.GameScope
+import ru.fredboy.cavedroid.domain.items.model.item.Item
+import ru.fredboy.cavedroid.domain.items.usecase.GetBlockByKeyUseCase
+import ru.fredboy.cavedroid.game.controller.mob.MobController
+import ru.fredboy.cavedroid.game.world.GameWorld
+import ru.fredboy.cavedroid.gameplay.controls.action.annotation.BindUseItemAction
+import javax.inject.Inject
+
+@GameScope
+@BindUseItemAction(UseCakeAction.ACTION_KEY)
+class UseCakeAction @Inject constructor(
+    private val gameWorld: GameWorld,
+    private val mobController: MobController,
+    private val getBlockByKeyUseCase: GetBlockByKeyUseCase,
+) : IUseItemAction {
+
+    override fun perform(item: Item.Usable, x: Int, y: Int): Boolean {
+        val cake = getBlockByKeyUseCase["cake_0"]
+
+        if (gameWorld.canPlaceToForeground(x, y, cake)) {
+            gameWorld.placeToForeground(x, y, cake)
+            mobController.player.decreaseCurrentItemCount()
+            return true
+        }
+
+        return false
+    }
+
+    companion object {
+        const val ACTION_KEY = "use_cake_action"
+    }
+}
