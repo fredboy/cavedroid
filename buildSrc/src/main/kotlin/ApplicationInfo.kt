e0d359cce3fcc37e8d6c52905354b2ce3fd3a5f8
fredboy <fredboy@protonmail.com>
2026-06-05 21:49:57 +0700

    Bump version to 1.2.4

--- buildSrc/src/main/kotlin/ApplicationInfo.kt
+++ buildSrc/src/main/kotlin/ApplicationInfo.kt
@@ -2,8 +2,8 @@ import org.gradle.api.JavaVersion
 
 object ApplicationInfo {
     const val name = "CaveDroid"
-    const val versionName = "1.2.3"
-    const val versionCode = 67
+    const val versionName = "1.2.4"
+    const val versionCode = 68
 
     const val packageName = "ru.fredboy.cavedroid"
 
--- core/common/src/main/kotlin/ru/fredboy/cavedroid/common/CaveDroidConstants.kt
+++ core/common/src/main/kotlin/ru/fredboy/cavedroid/common/CaveDroidConstants.kt
@@ -5,7 +5,7 @@ import java.util.Locale
 object CaveDroidConstants {
     const val TITLE = "CaveDroid"
 
-    const val VERSION = "1.2.3"
+    const val VERSION = "1.2.4"
 
     const val GITHUB_LINK = "https://github.com/fredboy/cavedroid"
 
